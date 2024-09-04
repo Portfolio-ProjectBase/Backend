d5dabca60496264c6293e324d0a352d2463d6413
Gökhan Asiltürk <gokhanasilturkk@gmail.com>
2024-09-04 19:09:58 +0300

    sql scripts added. gitignore updated.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='users' AND xtype='U')
+CREATE TABLE users (
+    id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    is_deleted BIT,
+    deleted_at DATETIME,
+    last_modified DATETIME,
+    created_date DATETIME,
+    name VARCHAR(255),
+    surname VARCHAR(255),
+    email_address VARCHAR(255),
+    password VARCHAR(255),
+    about_me VARCHAR(255),
+    detail VARCHAR(255)
+);
